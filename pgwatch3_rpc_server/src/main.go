0ac357577aa40bc4202a5f0d82486a2db8fb1eda
Akshat Jaimini <destrex271@gmail.com>
2024-06-05 07:38:08 +0530

    Registered Dummy Receiver functions

--- pgwatch3_rpc_server/src/main.go
+++ pgwatch3_rpc_server/src/main.go
@@ -1,3 +1,22 @@
 package main
 
-type Metric{}
+import (
+	"net"
+	"net/http"
+	"net/rpc"
+    "log"
+)
+
+func main(){
+    server := new(TextReceiver)
+    rpc.Register(server)
+    rpc.HandleHTTP()
+
+    listener, err := net.Listen("tcp", ":1234")
+
+    if err != nil{
+        log.Fatal(err)
+    }
+
+    http.Serve(listener, nil)
+}
