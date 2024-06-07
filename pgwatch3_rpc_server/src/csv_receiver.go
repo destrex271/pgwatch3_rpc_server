6d8c082220feef63ab6bbd7c80749c540529f39f
Akshat Jaimini <destrex271@gmail.com>
2024-06-07 16:08:24 +0530

    csv receiver formatted

--- pgwatch3_rpc_server/src/csv_receiver.go
+++ pgwatch3_rpc_server/src/csv_receiver.go
@@ -9,23 +9,7 @@ import(
 type CSVReceiver struct{}
 
 func (r *CSVReceiver) UpdateMetrics(measurement MeasurementMessage, reply *int){
-    // Write Metrics in a text file
-    file, err := os.Open("measurements.txt")
-    if err != nil{
-        log.Fatal("Error: Unable to open measurements file", err)
-    }
+    // Open/Create Output file
 
-    writer := bufio.NewWriter(file)
-
-    _, err = writer.Write([]byte("Name:" + measurement.DBName + "\nDescription: " + measurement.MetricName + "\n+======================================+"))
-
-    if err != nil{
-        log.Fatal("Unable to Write to file", err)
-    }
-
-    err = writer.Flush()
-    
-    if err != nil{
-        log.Fatal("Unable to Flush data to the file", err)
-    }
+    // Writing measurements to CSV
 }
