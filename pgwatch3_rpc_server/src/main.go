fc99916f2c1813026ba8321320fdfaa7f45d4371
Akshat Jaimini <destrex271@gmail.com>
2024-06-07 21:17:26 +0530

    primary receiver added

--- pgwatch3_rpc_server/src/main.go
+++ pgwatch3_rpc_server/src/main.go
@@ -5,11 +5,29 @@ import (
 	"net/http"
 	"net/rpc"
     "log"
+    "flag"
 )
 
+func setupRPCServer(){}
+
 func main(){
-    server := new(TextReceiver)
-    rpc.Register(server)
+    
+    receiverType := flag.String("type", "", "The type of sink that you want to keep this node as.\nAvailable options:\n\t- csv\n\t- text")
+    flag.Parse()
+
+    if *receiverType == "csv"{
+        server := new(CSVReceiver)
+        rpc.Register(server)
+    }else if *receiverType == "text"{
+        // Only for testing
+        server := new(TextReceiver)
+        rpc.Register(server)
+    }else{
+        // Throw Error
+        log.Fatal("No Sink Type was provided. Please use the --type option")
+        return
+    }
+
     rpc.HandleHTTP()
 
     listener, err := net.Listen("tcp", ":1234")
