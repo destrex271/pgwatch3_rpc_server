dccf57fdc58b68cd654b1ee9efd8d796c931c22f
Akshat Jaimini <destrex271@gmail.com>
2024-07-22 22:09:33 +0530

    Updated CSV Reciever: Adjusted Formatting in output file

--- pgwatch3_rpc_server/src/csv_receiver.go
+++ pgwatch3_rpc_server/src/csv_receiver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+    "encoding/json"
 )
 
 type CSVReceiver struct{}
@@ -47,33 +48,64 @@ func (r *CSVReceiver) UpdateMeasurements(msg *MeasurementMessage, logMsg *string
 	}
 
 	writer := csv.NewWriter(file)
+    // Add column Names
+    rec := [...]string{
+        "DBName", "SourceType", "MetricName", "Measurements", "CustomTags", "Metric Definitions", 
+    }
 
-	record := [...]string{
-		msg.DBName,
-		msg.SourceType,
-		msg.MetricName,
-		"CustomTags",
-		// TODO: This is a map, need to setup a different way to write this field, msg.CustomTags,
-		"Measurements",
-		// TODO: Measurements Object, need to setup a way to retrieve string version, Data
-		"Metrics",
-		// TODO: Metric field, need to setup a way to retrieve as string, MetricDef
-		msg.RealDbname,
-		msg.SystemIdentifier,
-	}
+    if err := writer.Write(rec[:]); err != nil {
+        log.Fatal("[ERROR]: Unable to write to CSV file " + fileName + "Error: " + err.Error())
+        return err
+    }
 
-	// Writing measurements to CSV
-	if err := writer.Write(record[:]); err != nil {
-		log.Fatal("Unable to write to CSV file " + fileName + "Error: " + err.Error())
-		return err
-	}
+    for _, data := range msg.Data{
+        record := [...]string{
+            msg.DBName,
+            msg.SourceType,
+            msg.MetricName,
+            createKeyValuePairs(data),
+            createKeyValuePairs_String(msg.CustomTags),
+            getJson(msg.MetricDef),
+        }
 
-	writer.Flush()
+        // Writing measurements to CSV
+        if err := writer.Write(record[:]); err != nil {
+            log.Fatal("Unable to write to CSV file " + fileName + "Error: " + err.Error())
+            return err
+        }
 
-	if err := writer.Error(); err != nil {
-		log.Fatal("Error: ", err)
-		return err
-	}
+        writer.Flush()
+
+        if err := writer.Error(); err != nil {
+            log.Fatal("Error: ", err)
+            return err
+        }
+    }
 
 	return nil
+}
+
+func createKeyValuePairs(m map[string]any) string {
+    jsonString, err := json.Marshal(m)
+    if err != nil {
+        log.Default().Fatal("Error converting map to JSON string:", err)
+    }
+    return string(jsonString)
+}
+
+
+func createKeyValuePairs_String(m map[string]string) string {
+    jsonString, err := json.Marshal(m)
+    if err != nil {
+        log.Default().Fatal("[ERROR]: Converting map to JSON string:", err)
+    }
+    return string(jsonString)
+}
+
+func getJson(m Metric) string {
+    jsonString, err := json.Marshal(m)
+    if err != nil{
+        log.Default().Fatal("[ERROR]: Unable to parse Metric Definition")
+    }
+    return string(jsonString)
 }
