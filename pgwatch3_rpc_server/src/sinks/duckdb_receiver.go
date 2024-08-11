09504050579e5dcb1c092dcb55312f7bd15ba3db
Akshat Jaimini <destrex271@gmail.com>
2024-08-11 16:05:05 +0530

    Added base duckdb receiver

--- pgwatch3_rpc_server/src/sinks/duckdb_receiver.go
+++ pgwatch3_rpc_server/src/sinks/duckdb_receiver.go
@@ -2,53 +2,33 @@ package sinks
 
 /**
 *
-* Similar to PostgresWriter in the main postgres repository: https://github.com/cybertec-postgresql/pgwatch3/blob/master/src/sinks/postgres.go
+* Takes a DuckDB Connection and writes to <DBNAME>.db file
 *
  */
 
 import (
-	"context"
-
 	"github.com/marcboeker/go-duckdb"
 )
 
-
-type DuckDBReceiver struct{
+type DuckDBReceiver struct {
 }
 
-type DuckDBWriter struct{
-   conn   duckdb.Connector
+type DuckDBWriter struct {
+	conn duckdb.Connector
 }
 
-func NewDuckDBWriter(dbname string) (*DuckDBWriter, error){
-    conn, err := duckdb.NewConnector(dbname + ".db", nil)
-    wr := new(DuckDBWriter)
+func NewDuckDBWriter(dbname string) (*DuckDBWriter, error) {
+	conn, err := duckdb.NewConnector(dbname+".db", nil)
+	wr := new(DuckDBWriter)
 
-    if err != nil{
-        return wr, err
-    }
+	if err != nil {
+		return wr, err
+	}
 
-    wr.conn = *conn
-    return wr, nil
+	wr.conn = *conn
+	return wr, nil
 }
 
-func (r *DuckDBReceiver) UpdateMeasurements(msg *MeasurementMessage, logMsg *string) error{ 
-
-    dbWriter, err := NewDuckDBWriter(msg.DBName)
-
-    if err != nil{
-        *logMsg = err.Error()
-        return err
-    }
-
-    // Get Connection
-    dbConn, err := dbWriter.conn.Connect(context.Background())
-
-    if err != nil{
-        *logMsg = err.Error()
-        return err
-    }
-    defer dbConn.Close()
-
-    return nil
+func (r *DuckDBReceiver) UpdateMeasurements(msg *MeasurementMessage, logMsg *string) error {
+	return nil
 }
