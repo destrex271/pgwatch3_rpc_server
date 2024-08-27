599a9f1f75f9908bf5f4da52cfbd06828e5d030f
Pavlo Golub <pavlo.golub@gmail.com>
2024-08-27 16:07:30 +0200

    remove unused consts

--- sinks/types.go
+++ sinks/types.go
@@ -4,13 +4,6 @@ import (
 	"github.com/cybertec-postgresql/pgwatch/v3/api"
 )
 
-const (
-	CSV     = 1
-	TEXT    = 2
-	PARQUET = 3
-	NONE    = -1
-)
-
 type SyncReq struct {
 	OPR        string
 	DBName     string
