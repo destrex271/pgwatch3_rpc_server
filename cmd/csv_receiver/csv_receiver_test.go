fa546c68a7afa41223d211e9ae8a87f542600165
Akshat Jaimini <destrex271@gmail.com>
2024-09-06 21:47:34 +0530

    Added CSV Receiever tests

---
+++ cmd/csv_receiver/csv_receiver_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cybertec-postgresql/pgwatch/v3/api"
+	"github.com/destrex271/pgwatch3_rpc_server/sinks"
+	"github.com/stretchr/testify/assert"
+)
+
+func getMeasurementEnvelope() *api.MeasurementEnvelope {
+	measurement := make(map[string]any)
+	measurement["cpu"] = "0.001"
+	measurement["checkpointer"] = "1"
+	var measurements []map[string]any
+	measurements = append(measurements, measurement)
+
+	sql := make(map[int]string)
+	sql[12] = "select * from abc;"
+	metrics := &api.Metric{
+		SQLs:        sql,
+		InitSQL:     "select * from abc;",
+		NodeStatus:  "healthy",
+		StorageName: "teststore",
+		Description: "test metric",
+	}
+
+	return &api.MeasurementEnvelope{
+		DBName:           "test",
+		SourceType:       "test_source",
+		MetricName:       "testMetric",
+		CustomTags:       nil,
+		Data:             measurements,
+		MetricDef:        *metrics,
+		RealDbname:       "test",
+		SystemIdentifier: "Identifier",
+	}
+}
+
+func TestUpdateMeasurements_CSV_ValidData(t *testing.T) {
+
+	fullPath, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	sync_handler := sinks.NewSyncMetricHandler(1024)
+
+	// Create new CSV Receiver
+	recv := &CSVReceiver{
+		FullPath:          fullPath,
+		SyncMetricHandler: sync_handler,
+	}
+
+	// Call Update Measurements with dummy packet
+	msg := getMeasurementEnvelope()
+	logMsg := new(string)
+	err = recv.UpdateMeasurements(msg, logMsg)
+
+	assert.Nil(t, err, *logMsg)
+
+	// Check folder structure and metric file
+
+	// Check if root folder created for database
+	if _, err := os.Stat(fullPath + "/" + msg.DBName); err != nil {
+		assert.False(t, os.IsNotExist(err), "Directory does not exist")
+	}
+
+	// Check if csv file for metric exists
+	assert.FileExists(t, fullPath+"/"+msg.DBName+"/"+msg.MetricName+".csv", "CSV file not found for metric")
+
+	// Cleanup
+	os.RemoveAll(fullPath + "/" + msg.DBName)
+}
--- go.mod
+++ go.mod
@@ -5,16 +5,18 @@ go 1.22.1
 require (
 	github.com/cybertec-postgresql/pgwatch/v3 v3.0.0-20240827142607-af4696bca143
 	github.com/parquet-go/parquet-go v0.23.0
+	github.com/segmentio/kafka-go v0.4.47
+	github.com/stretchr/testify v1.9.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
 	github.com/coreos/go-semver v0.3.1 // indirect
 	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/cybertec-postgresql/pgx-migrator v1.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -27,6 +29,7 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_golang v1.20.2 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
@@ -35,7 +38,6 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/segmentio/encoding v0.4.0 // indirect
-	github.com/segmentio/kafka-go v0.4.47 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.15 // indirect
