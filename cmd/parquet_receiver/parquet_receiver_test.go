8c5365b8eba87f42ed199f465fc32aa727579680
Akshat Jaimini <destrex271@gmail.com>
2024-09-17 21:51:29 +0530

    Added tests for parquet receiver (#24)
    * Added tests for parquet receiver
    * go mod tidy
    
    ---------
    
    Co-authored-by: Pavlo Golub <pavlo.golub@gmail.com>

---
+++ cmd/parquet_receiver/parquet_receiver_test.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cybertec-postgresql/pgwatch/v3/api"
+	"github.com/stretchr/testify/assert"
+)
+
+func getMeasurementEnvelope() *api.MeasurementEnvelope {
+	measurement := make(map[string]any)
+	measurement["cpu"] = "0.001"
+	measurement["checkpointer"] = "1"
+	var measurements []map[string]any
+	measurements = append(measurements, measurement)
+
+	sql := make(map[int]string)
+	sql[12] = "select * from abc;"
+	metrics := &api.Metric{
+		SQLs:        sql,
+		InitSQL:     "select * from abc;",
+		NodeStatus:  "healthy",
+		StorageName: "teststore",
+		Description: "test metric",
+	}
+
+	return &api.MeasurementEnvelope{
+		DBName:           "test",
+		SourceType:       "test_source",
+		MetricName:       "testMetric",
+		CustomTags:       nil,
+		Data:             measurements,
+		MetricDef:        *metrics,
+		RealDbname:       "test",
+		SystemIdentifier: "Identifier",
+	}
+}
+
+func TestUpdateMeasurements_PARQ(t *testing.T) {
+	// Get path
+	fullPath, err := os.Getwd()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	// Create Parquet Receiver
+	recv := &ParqReceiver{
+		FullPath: fullPath,
+	}
+	msg := getMeasurementEnvelope()
+	logMsg := new(string)
+	err = recv.UpdateMeasurements(msg, logMsg)
+
+	// Check if there were any errors while updating measurements
+	assert.Nil(t, err, "Received unexpected error")
+
+	// Check folder structure
+	if _, err := os.Stat(fullPath + "/parquet_readings"); err != nil {
+		assert.False(t, os.IsNotExist(err), "Directory does not exist")
+	}
+
+	// Check if csv file for metric exists
+	assert.FileExists(t, fullPath+"/parquet_readings/"+msg.DBName+".parquet", "CSV file not found for metric")
+
+	// Cleanup
+	os.RemoveAll(fullPath + "/parquet_readings")
+}
+
+func TestUpdateMeasurements_PARQ_EmptyDBName(t *testing.T) {
+	// Get path
+	fullPath, err := os.Getwd()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	// Create Parquet Receiver
+	recv := &ParqReceiver{
+		FullPath: fullPath,
+	}
+	msg := getMeasurementEnvelope()
+	msg.DBName = ""
+	logMsg := new(string)
+	err = recv.UpdateMeasurements(msg, logMsg)
+
+	// Check if there were any errors while updating measurements
+	assert.EqualError(t, err, "Empty Database", "Error Message not thrown for empty database name")
+
+	// Check if directories were not created
+	if _, err := os.Stat(fullPath + "/parquet_readings"); err != nil {
+		assert.True(t, os.IsNotExist(err), "Directory does not exist")
+	}
+}
+
+func TestUpdateMeasurements_PARQ_EmptyMetricName(t *testing.T) {
+	// Get path
+	fullPath, err := os.Getwd()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	// Create Parquet Receiver
+	recv := &ParqReceiver{
+		FullPath: fullPath,
+	}
+	msg := getMeasurementEnvelope()
+	msg.MetricName = ""
+	logMsg := new(string)
+	err = recv.UpdateMeasurements(msg, logMsg)
+
+	// Check if there were any errors while updating measurements
+	assert.EqualError(t, err, "Empty Metric Name", "Error Message not thrown for empty metric name")
+
+	// Check if directories were not created
+	if _, err := os.Stat(fullPath + "/parquet_readings"); err != nil {
+		assert.True(t, os.IsNotExist(err), "Directory does not exist")
+	}
+}
