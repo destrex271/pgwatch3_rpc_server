d119e2e0e9271637c1e5bc33bf3e900abb8b44c1
Akshat Jaimini <destrex271@gmail.com>
2024-09-24 19:25:04 +0530

    Added add bucket and check bucket functions

--- cmd/s3_receiver/s3_receiver.go
+++ cmd/s3_receiver/s3_receiver.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"context"
+	"errors"
+	"log"
 
 	"github.com/cybertec-postgresql/pgwatch/v3/api"
 	"github.com/destrex271/pgwatch3_rpc_server/sinks"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/smithy-go"
 )
 
 type S3Receiver struct {
-	client *s3.Client
+	S3Client *s3.Client
+	Ctx      context.Context
 	sinks.SyncMetricHandler
 }
 
 func NewS3Receiver(awsEndpoint string, awsRegion string) (*S3Receiver, error) {
 	awsCfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithRegion(awsRegion),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("test", "test", "")),
 	)
 	if err != nil {
 		return nil, err
@@ -31,14 +38,51 @@ func NewS3Receiver(awsEndpoint string, awsRegion string) (*S3Receiver, error) {
 	})
 
 	recv := &S3Receiver{
-		client:            client,
+		S3Client:          client,
+		Ctx:               context.Background(),
 		SyncMetricHandler: sinks.NewSyncMetricHandler(1024),
 	}
 
 	return recv, nil
 }
 
-func (s3 *S3Receiver) UpdateMeasurements(msg *api.MeasurementEnvelope, logMsg *string) error {
+func (r *S3Receiver) AddDatabase(dbname string) error {
+	// Each Bucket stores all metrics for one database
+	if _, err := r.S3Client.CreateBucket(r.Ctx, &s3.CreateBucketInput{
+		Bucket: &dbname,
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *S3Receiver) BucketExists(bucketName string) (bool, error) {
+	_, err := r.S3Client.HeadBucket(context.TODO(), &s3.HeadBucketInput{
+		Bucket: aws.String(bucketName),
+	})
+	exists := true
+	if err != nil {
+		var apiError smithy.APIError
+		if errors.As(err, &apiError) {
+			switch apiError.(type) {
+			case *types.NotFound:
+				log.Printf("Bucket %v is available.\n", bucketName)
+				exists = false
+				err = nil
+			default:
+				log.Printf("Either you don't have access to bucket %v or another error occurred. "+
+					"Here's what happened: %v\n", bucketName, err)
+			}
+		}
+	} else {
+		log.Printf("Bucket %v exists and you already own it.", bucketName)
+	}
+
+	return exists, err
+}
+
+func (r *S3Receiver) UpdateMeasurements(msg *api.MeasurementEnvelope, logMsg *string) error {
 
 	return nil
 }
--- cmd/s3_receiver/s3_receiver_test.go
+++ cmd/s3_receiver/s3_receiver_test.go
@@ -55,5 +55,48 @@ func TestNewS3Receiver(t *testing.T) {
 
 	assert.Nil(t, err, "error encoutnered while creating S3Receiver")
 	assert.NotNil(t, client, "received nil instead of client")
+}
 
+func TestAddDatabase(t *testing.T) {
+
+	ctx := context.Background()
+
+	container, err := initContainer(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := container.Terminate(ctx); err != nil {
+			log.Println("unable to terminate localstack container : " + err.Error())
+		}
+	}()
+
+	mappedPort, err := container.MappedPort(ctx, nat.Port("4566/tcp"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	provider, err := testcontainers.NewDockerProvider()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer provider.Close()
+
+	host, err := provider.DaemonHost(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client, err := NewS3Receiver(fmt.Sprintf("http://%s:%d", host, mappedPort.Int()), "us-east-1")
+
+	assert.Nil(t, err, "error encoutnered while creating S3Receiver")
+	assert.NotNil(t, client, "received nil instead of client")
+
+	dbname := "test-db"
+	client.AddDatabase(dbname)
+
+	res, err := client.BucketExists(dbname)
+
+	assert.Nil(t, err, "error encountered while checking bucket")
+	assert.True(t, res, "bucket should exist")
 }
