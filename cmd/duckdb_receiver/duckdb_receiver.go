9c438bcad3fcd590684f963f48bad2360bb109e7
bassamadnan <bassamadnan2012@gmail.com>
2025-02-18 19:23:33 +0530

    add insert measurements duckdb receiver

--- cmd/duckdb_receiver/duckdb_receiver.go
+++ cmd/duckdb_receiver/duckdb_receiver.go
@@ -11,15 +11,19 @@ import (
 
 	"github.com/cybertec-postgresql/pgwatch/v3/api"
 	"github.com/destrex271/pgwatch3_rpc_server/sinks"
+	"github.com/marcboeker/go-duckdb"
 )
 
 type DuckDBReceiver struct {
-	Ctx  context.Context
-	Conn *sql.DB
+	Ctx       context.Context
+	Conn      *sql.DB
+	DBName    string
+	TableName string
 	sinks.SyncMetricHandler
 }
 
 func (dbr *DuckDBReceiver) initializeTable() {
+	// TODO: change below "measurements" to dbr.TableName
 	_, err := dbr.Conn.Exec("CREATE TABLE IF NOT EXISTS measurements(dbname VARCHAR, metric_name VARCHAR, data JSON, custom_tags JSON, metric_def JSON, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (dbname, timestamp))")
 	if err != nil {
 		log.Fatal(err)
@@ -29,7 +33,6 @@ func (dbr *DuckDBReceiver) initializeTable() {
 
 func NewDBDuckReceiver(databaseName string) (dbr *DuckDBReceiver, err error) {
 	db, err := sql.Open("duckdb", databaseName)
-
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
@@ -37,6 +40,8 @@ func NewDBDuckReceiver(databaseName string) (dbr *DuckDBReceiver, err error) {
 
 	dbr = &DuckDBReceiver{
 		Conn:              db,
+		DBName:            databaseName,
+		TableName:         "measurements",
 		SyncMetricHandler: sinks.NewSyncMetricHandler(1024),
 	}
 
@@ -44,11 +49,63 @@ func NewDBDuckReceiver(databaseName string) (dbr *DuckDBReceiver, err error) {
 	return dbr, nil
 }
 
+// type MeasurementEnvelope struct {
+// 	DBName           string
+// 	SourceType       string
+// 	MetricName       string
+// 	CustomTags       map[string]string
+// 	Data             Measurements
+// 	MetricDef        Metric
+// 	RealDbname       string
+// 	SystemIdentifier string
+// }
+
 func (r *DuckDBReceiver) InsertMeasurements(data *api.MeasurementEnvelope, ctx context.Context) error {
 	fmt.Print("DATA RECIEVED: ", "\n\n\n")
 	metricDef, _ := json.Marshal(data.MetricDef)
 	fmt.Println(string(metricDef))
+	customTagsJSON, _ := json.Marshal(data.CustomTags)
+	// measurementJSON, _ := json.Marshal(data.Data)
 
+	// https://duckdb.org/docs/clients/go.html
+	// https://github.com/marcboeker/go-duckdb/blob/main/examples/appender/main.go
+	// read more on this https://pkg.go.dev/github.com/marcboeker/go-duckdb@v1.8.4#NewConnector
+	connector, err := duckdb.NewConnector(r.DBName, nil)
+	if err != nil {
+		log.Print("Error: ", err)
+		return err
+	}
+	conn, err := connector.Connect(context.Background())
+	if err != nil {
+		log.Print("Error: ", err)
+		return err
+	}
+	defer conn.Close()
+	appender, err := duckdb.NewAppenderFromConn(conn, "", r.TableName)
+	if err != nil {
+		log.Print("Error: ", err)
+		return err
+	}
+	defer appender.Close()
+	for _, measurement := range data.Data {
+		measurementJSON, err := json.Marshal(measurement)
+		if err != nil {
+			log.Print("failed to marshal measurement: ", err)
+			return err
+		}
+
+		if err := appender.AppendRow(
+			data.DBName,
+			data.MetricName,
+			string(measurementJSON),
+			string(customTagsJSON),
+			string(metricDef),
+			time.Now(),
+		); err != nil {
+			log.Print("could not append row: ", err)
+			return err
+		}
+	}
 	return nil
 }
 
