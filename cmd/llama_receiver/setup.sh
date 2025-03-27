8162e963762f37ab757a4c4bb58ab81367b08644
Akshat Jaimini <destrex271@gmail.com>
2025-03-27 14:22:44 +0530

    Delete cmd/llama_receiver/setup.sh

--- cmd/llama_receiver/setup.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-docker rm ollama || true
-
-docker pull ollama/ollama
-
-
-# start new ollama container
-docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
