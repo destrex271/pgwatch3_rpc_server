205d6e5741cc8cb4a089abe2dc8c1e420f8c2eec
0xgouda <gouda0x@gmail.com>
2025-07-08 18:22:49 +0300

    [+] update text sink to use gRPC protobuf types and methods signatures

--- cmd/text_receiver/text_receiver.go
+++ cmd/text_receiver/text_receiver.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/destrex271/pgwatch3_rpc_server/sinks"
-
-	"github.com/cybertec-postgresql/pgwatch/v3/api"
+	"github.com/destrex271/pgwatch3_rpc_server/sinks/pb"
 )
 
 type TextReceiver struct {
@@ -27,35 +27,36 @@ func NewTextReceiver(fullPath string) (tr *TextReceiver) {
 	return tr
 }
 
-func (r TextReceiver) UpdateMeasurements(msg *api.MeasurementEnvelope, logMsg *string) error {
+func (r TextReceiver) UpdateMeasurements(ctx context.Context, msg *pb.MeasurementEnvelope) (*pb.Reply, error) {
 	if err := sinks.IsValidMeasurement(msg); err != nil {
-		return  err
+		return nil, err
 	}
 
 	// Write Metrics in a text file
-	fileName := msg.DBName + ".txt"
+	fileName := msg.GetDBName() + ".txt"
 	file, err := os.OpenFile(r.FullPath+"/"+fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	if err != nil {
-		*logMsg = "Unable to open file. Error: " + err.Error()
-		log.Println(*logMsg)
-		return err
+		log.Println("Unable to open file. Error: " + err.Error())
+		return nil, err
 	}
 
 	writer := bufio.NewWriter(file)
 	defer func() {_ = file.Close()}()
 
-	output := "DBName: " + msg.DBName + "\n" + "Metric: " + msg.MetricName + "\n"
+	output := "DBName: " + msg.GetDBName() + "\n" + "Metric: " + msg.GetMetricName() + "\n"
 
-	for _, data := range msg.Data {
-		output += sinks.GetJson(data) + "\n"
+	for _, measurement := range msg.GetData() {
+		output += sinks.GetJson(measurement) + "\n"
 	}
 
 	output += "\n===================================\n"
 
 	_, err = fmt.Fprintln(writer, output)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return writer.Flush()
+
+	err = writer.Flush()
+	return &pb.Reply{}, err
 }
--- cmd/text_receiver/text_receiver_test.go
+++ cmd/text_receiver/text_receiver_test.go
@@ -1,67 +1,42 @@
 package main
 
 import (
+	"context"
 	"os"
 	"testing"
 
-	"github.com/cybertec-postgresql/pgwatch/v3/api"
-	"github.com/destrex271/pgwatch3_rpc_server/sinks"
+	"github.com/destrex271/pgwatch3_rpc_server/sinks/pb"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
-func getMeasurementEnvelope() *api.MeasurementEnvelope {
-	measurement := make(map[string]any)
-	measurement["cpu"] = "0.001"
-	measurement["checkpointer"] = "1"
-	var measurements []map[string]any
-	measurements = append(measurements, measurement)
-
-	sql := make(map[int]string)
-	sql[12] = "select * from abc;"
-	metrics := &api.Metric{
-		SQLs:        sql,
-		InitSQL:     "select * from abc;",
-		NodeStatus:  "healthy",
-		StorageName: "teststore",
-		Description: "test metric",
+func GetTestMeasurementEnvelope() *pb.MeasurementEnvelope {
+	st, err := structpb.NewStruct(map[string]any{"key": "val"})
+	if err != nil {
+		panic(err)
 	}
-
-	return &api.MeasurementEnvelope{
+	measurements := []*structpb.Struct{st}
+	return &pb.MeasurementEnvelope{
 		DBName:           "test",
-		SourceType:       "test_source",
 		MetricName:       "testMetric",
-		CustomTags:       nil,
 		Data:             measurements,
-		MetricDef:        *metrics,
-		RealDbname:       "test",
-		SystemIdentifier: "Identifier",
 	}
 }
 
 func TestUpdateMeasurements(t *testing.T) {
 	path, err := os.Getwd()
 	if err != nil {
-		panic("Error: " + err.Error())
+		panic(err)
 	}
 
-	// Get TextReceiver
-	recv := &TextReceiver{
-		FullPath:          path,
-		SyncMetricHandler: sinks.NewSyncMetricHandler(1024),
-	}
-
-	logMsg := new(string)
-	msg := getMeasurementEnvelope()
-	err = recv.UpdateMeasurements(msg, logMsg)
+	recv := NewTextReceiver(path)
+	msg := GetTestMeasurementEnvelope()
+	_, err = recv.UpdateMeasurements(context.Background(), msg)
 
 	// Check if there are any errors
-	assert.Nil(t, err, "Error encountered while updating measurements")
+	assert.NoError(t, err, "Error encountered while updating measurements")
 
 	// Check if file was created
-	assert.FileExists(t, path+"/"+msg.DBName+".txt", "Database file does not exist")
-
-	err = os.Remove(path + "/" + msg.DBName + ".txt")
-	if err != nil {
-		t.Error(err)
-	}
+	assert.FileExists(t, path + "/" + msg.DBName + ".txt", "Database file does not exist")
+	_ = os.Remove(path + "/" + msg.DBName + ".txt")
 }
