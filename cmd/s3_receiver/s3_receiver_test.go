1ae7acda94f6bf40990e73225c51d49c1e3a45cf
0xgouda <gouda0x@gmail.com>
2025-07-09 14:37:48 +0300

    [+] update s3 sink to use gRPC protobuf types and methods signatures

--- cmd/s3_receiver/s3_receiver_test.go
+++ cmd/s3_receiver/s3_receiver_test.go
@@ -6,39 +6,25 @@ import (
 	"log"
 	"testing"
 
-	"github.com/cybertec-postgresql/pgwatch/v3/api"
+	
+	"github.com/destrex271/pgwatch3_rpc_server/sinks/pb"
 	"github.com/docker/go-connections/nat"
 	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/localstack"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
-func getMeasurementEnvelope() *api.MeasurementEnvelope {
-	measurement := make(map[string]any)
-	measurement["cpu"] = "0.001"
-	measurement["checkpointer"] = "1"
-	var measurements []map[string]any
-	measurements = append(measurements, measurement)
-
-	sql := make(map[int]string)
-	sql[12] = "select * from abc;"
-	metrics := &api.Metric{
-		SQLs:        sql,
-		InitSQL:     "select * from abc;",
-		NodeStatus:  "healthy",
-		StorageName: "teststore",
-		Description: "test metric",
+func GetTestMeasurementEnvelope() *pb.MeasurementEnvelope {
+	st, err := structpb.NewStruct(map[string]any{"key": "val"})
+	if err != nil {
+		panic(err)
 	}
-
-	return &api.MeasurementEnvelope{
+	measurements := []*structpb.Struct{st}
+	return &pb.MeasurementEnvelope{
 		DBName:           "test",
-		SourceType:       "test_source",
 		MetricName:       "testMetric",
-		CustomTags:       nil,
 		Data:             measurements,
-		MetricDef:        *metrics,
-		RealDbname:       "test",
-		SystemIdentifier: "Identifier",
 	}
 }
 
@@ -51,8 +37,7 @@ func initContainer(ctx context.Context) (*localstack.LocalStackContainer, error)
 	return localstackContainer, err
 }
 
-func TestNewS3Receiver(t *testing.T) {
-
+func TestS3Receiver(t *testing.T) {
 	ctx := context.Background()
 
 	container, err := initContainer(ctx)
@@ -67,108 +52,43 @@ func TestNewS3Receiver(t *testing.T) {
 
 	mappedPort, err := container.MappedPort(ctx, nat.Port("4566/tcp"))
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
 
 	provider, err := testcontainers.NewDockerProvider()
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
 	defer func() {_ = provider.Close()}()
 
 	host, err := provider.DaemonHost(ctx)
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
 
 	client, err := NewS3Receiver(fmt.Sprintf("http://%s:%d", host, mappedPort.Int()), "us-east-1", "test", "test")
-
-	assert.Nil(t, err, "error encoutnered while creating S3Receiver")
-	assert.NotNil(t, client, "received nil instead of client")
-}
-
-func TestAddDatabase(t *testing.T) {
-
-	ctx := context.Background()
-
-	container, err := initContainer(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		if err := container.Terminate(ctx); err != nil {
-			log.Println("unable to terminate localstack container : " + err.Error())
-		}
-	}()
-
-	mappedPort, err := container.MappedPort(ctx, nat.Port("4566/tcp"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	provider, err := testcontainers.NewDockerProvider()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {_ = provider.Close()}()
-
-	host, err := provider.DaemonHost(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	client, err := NewS3Receiver(fmt.Sprintf("http://%s:%d", host, mappedPort.Int()), "us-east-1", "test", "test")
-
-	assert.Nil(t, err, "error encoutnered while creating S3Receiver")
+	assert.NoError(t, err, "error encountered while creating S3Receiver")
 	assert.NotNil(t, client, "received nil instead of client")
 
-	dbname := "test-db"
-	err = client.AddDatabase(dbname)
-	assert.NoError(t, err)
+	t.Run("Add Database", func(t *testing.T) {
+		dbname := "test-db"
+		err = client.AddDatabase(dbname)
+		assert.NoError(t, err)
 
-	res, err := client.DBExists(dbname)
+		res, err := client.DBExists(dbname)
+		assert.NoError(t, err, "error encountered while checking bucket")
+		assert.True(t, res, "bucket should exist")
+	})
+	
+	t.Run("Update Measurements", func(t *testing.T) {
+		msg := GetTestMeasurementEnvelope()
+		_, err = client.UpdateMeasurements(ctx, msg)
+		assert.NoError(t, err, "error encountered while updating measurements")
 
-	assert.Nil(t, err, "error encountered while checking bucket")
-	assert.True(t, res, "bucket should exist")
-}
-
-func TestUpdateMeasurements(t *testing.T) {
-	ctx := context.Background()
-
-	container, err := initContainer(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		if err := container.Terminate(ctx); err != nil {
-			log.Println("unable to terminate localstack container : " + err.Error())
-		}
-	}()
-
-	mappedPort, err := container.MappedPort(ctx, nat.Port("4566/tcp"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	provider, err := testcontainers.NewDockerProvider()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {_ = provider.Close()}()
-
-	host, err := provider.DaemonHost(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	client, err := NewS3Receiver(fmt.Sprintf("http://%s:%d", host, mappedPort.Int()), "us-east-1", "test", "test")
-
-	assert.Nil(t, err, "error encoutnered while creating S3Receiver")
-	assert.NotNil(t, client, "received nil instead of client")
-
-	msg := getMeasurementEnvelope()
-	logMsg := new(string)
-	err = client.UpdateMeasurements(msg, logMsg)
-
-	assert.Nil(t, err, "error encountered while updating measurements")
+		newCtx, cancel := context.WithCancel(ctx)
+		cancel()
+		reply, err := client.UpdateMeasurements(newCtx, msg)
+		assert.Equal(t, reply.GetLogmsg(), "context cancelled, stopping writer...")
+		assert.NoError(t, err)
+	})
 }
